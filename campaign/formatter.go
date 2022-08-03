11f74f0d0ed879c2870791b247d5ad657f063acf
Adnan Isnain <adnan.isnain@gmail.com>
2022-08-03 23:06:59 +0700

    Formatting campaigns

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,37 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserId           int    `json:"user_id"`
+	Name             string `json:"title"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmout        int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func formatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{
+		ID:               campaign.ID,
+		UserId:           campaign.UserID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		ImageURL:         "",
+		GoalAmout:        campaign.GoalAmout,
+		CurrentAmount:    campaign.CurrentAmount,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageURL = campaign.CampaignImages[0].FileName
+	}
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	allCampaigns := []CampaignFormatter{}
+	for _, campaign := range campaigns {
+		allCampaigns = append(allCampaigns, formatCampaign(campaign))
+	}
+
+	return allCampaigns
+}
